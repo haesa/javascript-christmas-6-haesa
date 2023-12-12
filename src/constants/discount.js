6b27fe934a651eef7718123c24c99b2b2ab0797e
haesa <xdhaesa@gmail.com>
2023-12-13 04:52:24 +0900

    [Refactor]: EVENT_DATE 상수 객체 얼리기

--- src/constants/discount.js
+++ src/constants/discount.js
@@ -5,11 +5,11 @@ const DISCOUNT = Object.freeze({
   specialDiscount: 1_000,
 });
 
-const EVENT_DATE = {
+const EVENT_DATE = Object.freeze({
   christmas: (date) => date <= 25,
   weekend: (date) => new Set([1, 2]).has(date % 7),
   weekday: (date) => new Set([3, 4, 5, 6, 0]).has(date % 7),
   special: (date) => new Set([3, 10, 17, 24, 25, 31]).has(date),
-};
+});
 
 export { DISCOUNT, EVENT_DATE };
